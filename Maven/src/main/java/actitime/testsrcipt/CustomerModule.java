0f5726689ed41156beb7b1ee0e6a1a48a8fa3cc0
Thejashwini <Thejashwini@DESKTOP-MIFDNCE>
2024-01-05 13:21:49 +0530

    .

---
+++ Maven/src/main/java/actitime/testsrcipt/CustomerModule.java
@@ -0,0 +1,9 @@
+package actitime.testsrcipt;
+
+import com.google.common.annotations.VisibleForTesting;
+
+public class CustomerModule {
+	@
+	
+
+}
---
+++ Maven/src/test/java/com/mav/testscripts/customermodule.java
@@ -0,0 +1,13 @@
+package com.mav.testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class customermodule {
+	@Test
+	public void createcustomer() {
+		Reporter.log("createcustomer",true);
+		
+	}
+
+}
